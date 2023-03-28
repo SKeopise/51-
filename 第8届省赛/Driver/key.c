5d6ecdada6bdc0a20a86d519515d222635f6fa38
AbyssalSkadi <2258895692@qq.com>
2023-03-28 18:53:05 +0800

    第八届修改为独立按键

--- 第8届省赛/Driver/key.c
+++ 第8届省赛/Driver/key.c
@@ -3,16 +3,9 @@
 
 
 
-unsigned char code Key_Map[4][4] = {
-	{'7','0','0','0'},
-	{'6','0','0','0'},
-	{'5','0','0','0'},
-	{'4','0','0','0'},	
-};
+unsigned char code Key_Map[4] = {'7','6','5','4'};
 
-unsigned char Key_Sta[4][4] = {
-	{1,1,1,1},{1,1,1,1},{1,1,1,1},{1,1,1,1}
-};
+unsigned char Key_Sta[4] = {1,1,1,1};
 
 
 
@@ -70,28 +63,23 @@ void KeyAction1(unsigned char keycode)
 
 void KeyDriver()
 {
-	unsigned char i,j;
-	static unsigned char Key_Backup[4][4] = {
-		{1,1,1,1},{1,1,1,1},{1,1,1,1},{1,1,1,1}
-	};
+	unsigned char i;
+	static unsigned char Key_Backup[4] = {1,1,1,1};
 	
 	for(i=0;i<4;i++)
 	{
-		for(j=0;j<4;j++)
+		if(Key_Backup[i] != Key_Sta[i])
 		{
-			if(Key_Backup[i][j] != Key_Sta[i][j])
+			if(Key_Backup[i] != 0)
 			{
-				if(Key_Backup[i][j] != 0)
-				{
-					KeyAction0(Key_Map[i][j]);
-				}
-				else
-				{
-					KeyAction1(Key_Map[i][j]);
-				}
-				
-				Key_Backup[i][j] = Key_Sta[i][j]; 
+				KeyAction0(Key_Map[i]);
 			}
+			else
+			{
+				KeyAction1(Key_Map[i]);
+			}
+			
+			Key_Backup[i] = Key_Sta[i]; 
 		}
 	}
 }
@@ -103,41 +91,31 @@ void KeyScan()
 {
 	unsigned char i =0;
 	static unsigned char Key_Out = 0;
-	static unsigned char Key_Buff[4][4] = {
-		{0xFF,0xFF,0xFF,0xFF},{0xFF,0xFF,0xFF,0xFF},
-		{0xFF,0xFF,0xFF,0xFF},{0xFF,0xFF,0xFF,0xFF}
-	};
+	static unsigned char Key_Buff[4]= {0xFF,0xFF,0xFF,0xFF};
 	
-	Key_Buff[Key_Out][0] = (Key_Buff[Key_Out][0] << 1) | KEY_IN_0;
-	Key_Buff[Key_Out][1] = (Key_Buff[Key_Out][1] << 1) | KEY_IN_1;
-	Key_Buff[Key_Out][2] = (Key_Buff[Key_Out][2] << 1) | KEY_IN_2;
-	Key_Buff[Key_Out][3] = (Key_Buff[Key_Out][3] << 1) | KEY_IN_3;
 
-	for(i=0;i<4;i++)
+	switch(Key_Out)
 	{
-		if((Key_Buff[Key_Out][i] & 0x0F) == 0x00)
-		{
-			Key_Sta[Key_Out][i] = 0;
-		}
-		else if((Key_Buff[Key_Out][i] & 0x0F) == 0x0F)
-		{
-			Key_Sta[Key_Out][i] = 1;
-		}
+		case 0: Key_Buff[0] = (Key_Buff[0] << 1) | KEY_OUT_0;break;
+		case 1: Key_Buff[1] = (Key_Buff[1] << 1) | KEY_OUT_1;break;
+		case 2: Key_Buff[2] = (Key_Buff[2] << 1) | KEY_OUT_2;break;
+		case 3: Key_Buff[3] = (Key_Buff[3] << 1) | KEY_OUT_3;break;
+		default: break;
+	}
+
+	if((Key_Buff[Key_Out] & 0x0F) == 0x00)
+	{
+		Key_Sta[Key_Out] = 0;
+	}
+	else if((Key_Buff[Key_Out] & 0x0F) == 0x0F)
+	{
+		Key_Sta[Key_Out] = 1;
 	}
 	
 	Key_Out++;
 	if(Key_Out > 3)
 	{
 		Key_Out = 0;
-	}
-	
-	switch(Key_Out)
-	{
-		case 0: KEY_OUT_0 = 0; KEY_OUT_3 = 1; break;
-		case 1: KEY_OUT_1 = 0; KEY_OUT_0 = 1; break;
-		case 2: KEY_OUT_2 = 0; KEY_OUT_1 = 1; break;
-		case 3: KEY_OUT_3 = 0; KEY_OUT_2 = 1; break;
-		default: break;
 	}
 }
 
--- 第8届省赛/Driver/key.h
+++ 第8届省赛/Driver/key.h
@@ -29,10 +29,6 @@ sbit KEY_OUT_1 = P3^1;
 sbit KEY_OUT_2 = P3^2;
 sbit KEY_OUT_3 = P3^3;
 
-sbit KEY_IN_0 = P4^4;
-sbit KEY_IN_1 = P4^2;
-sbit KEY_IN_2 = P3^5;
-sbit KEY_IN_3 = P3^4;
 
 
 
